d5c39d1c0fd8e1be75a443ed6f67dff333b00773
seymzr <seymzr@gmail.com>
2024-03-02 14:34:41 +0400

    s

---
+++ src/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <h2 className='text-6xl font-bold text-center text-gray-700 py-24'>About us</h2>
+
+
+    </div>
+  )
+}
+
+export default About
---
+++ src/Team.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Team = () => {
+  return (
+    <div>
+
+        <h2 className='text-6xl font-bold text-center text-gray-700  py-24'>Bright Brains behind this idea</h2>
+    </div>
+  )
+}
+
+export default Team
